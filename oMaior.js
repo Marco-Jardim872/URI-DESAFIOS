66655a7904264c6c9f7365178916ade305bcf620
Marco Jardim <70395937+Marco-Jardim872@users.noreply.github.com>
2021-08-16 02:30:37 -0300

    Add files via upload

---
+++ oMaior.js
@@ -0,0 +1,12 @@
+var input = require('fs').readFileSync('/dev/stdin', 'utf8');
+var lines = input.split(' ');
+
+
+let a = parseInt(lines.shift());
+let b = parseInt(lines.shift());
+let c = parseInt(lines.shift());
+
+let maiorAB = (a+b+Math.abs(a-b))/2;
+let maiorC = (maiorAB+c+Math.abs(maiorAB-c))/2;
+
+console.log(maiorC + " eh o maior");
